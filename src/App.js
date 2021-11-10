808072dfb97d81fe56c5222ab47ea26387ccd9b1
Abhishek Kumar <chocolate.ladu@gmail.com>
2021-11-10 12:31:32 +0530

    Notification page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './App.css';
 import Welcome from './pages/Welcome';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import PlanLayout from './pages/Layouts/PlanLayout';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import CodeConfirm from './pages/CodeConfirm';
+import AllowNotification from './pages/AllowNotification';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +13,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Welcome />}/>
           <Route exact path="/invite" element={<PhoneConfirmation />}/>
+          <Route exact path="/codeconfirm" element={<CodeConfirm />}/>
+          <Route exact path="/allow_notification" element={<AllowNotification/>} />
         </Routes>
       </PlanLayout>
     </BrowserRouter>
