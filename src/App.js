aef65ec1610bf267f63c4ec15fa6f8e881979154
Abhishek Kumar <chocolate.ladu@gmail.com>
2021-11-12 12:40:04 +0530

    Homepage

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import './App.css';
 import Welcome from './pages/Welcome';
 import PhoneConfirmation from './pages/PhoneConfirmation';
 import PlanLayout from './pages/Layouts/PlanLayout';
+import AppLayout from './pages/Layouts/AppLayout';
 import CodeConfirm from './pages/CodeConfirm';
+import Home from './pages/Home';
 import AllowNotification from './pages/AllowNotification';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <PlanLayout>
-        <Routes>
-          <Route exact path="/" element={<Welcome />}/>
-          <Route exact path="/invite" element={<PhoneConfirmation />}/>
-          <Route exact path="/codeconfirm" element={<CodeConfirm />}/>
-          <Route exact path="/allow_notification" element={<AllowNotification/>} />
-        </Routes>
-      </PlanLayout>
-    </BrowserRouter>
+    // <>
+      <BrowserRouter>
+        <PlanLayout>
+          <Routes>
+            <Route exact path="/" element={<Welcome />}/>
+            <Route exact path="/invite" element={<PhoneConfirmation />}/>
+            <Route exact path="/codeconfirm" element={<CodeConfirm />}/>
+            <Route exact path="/allow_notification" element={<AllowNotification/>} />
+          </Routes>
+        </PlanLayout>
+        <AppLayout>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </AppLayout>
+      </BrowserRouter>
   );
 }
 
